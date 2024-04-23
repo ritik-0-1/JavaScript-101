602a4c8d7128eda9ceb162d26eca69a87ac35a94
ritik-0-1 <ritik.kr775@gmail.com>
2024-04-23 12:53:22 +0530

    JS Basics

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+if(js === "amazing") alert("Javascript is FUN");
+
+40+8+23-10;  
+console.log( 40+8+23-10 );
+
+
+
+let javascript = true;
+console.log(javascript);
+
+console.log(typeof true);
+console.log(typeof 29);
+console.log(typeof "Jonas");
+console.log(typeof "23");
+
+
+console.log(typeof undefined);
