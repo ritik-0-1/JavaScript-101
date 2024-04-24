dff083c89fad3e203b8316a484359f009bde50ec
ritik-0-1 <ritik.kr775@gmail.com>
2024-04-24 11:23:54 +0530

    JS daily 24

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,59 @@
-let js = "amazing";
-if(js === "amazing") alert("Javascript is FUN");
+// let js = "amazing";
+// if(js === "amazing") alert("Javascript is FUN");
 
-40+8+23-10;  
-console.log( 40+8+23-10 );
-
-
-
-let javascript = true;
-console.log(javascript);
-
-console.log(typeof true);
-console.log(typeof 29);
-console.log(typeof "Jonas");
-console.log(typeof "23");
+// 40+8+23-10;  
+// console.log( 40+8+23-10 );
 
 
-console.log(typeof undefined);
+
+// let javascript = true;
+// console.log(javascript);
+
+// console.log(typeof true);
+// console.log(typeof 29);
+// console.log(typeof "Jonas");
+// console.log(typeof "23");
+
+// let year;
+// console.log(year);
+// console.log(typeof year );
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null); //error in JS that null is object -> legacy reasons
+
+// let firstName = "Ritik";
+// console.log(firstName);
+
+// firstName = "Jonas";
+// console.log(firstName);
+
+const now = 2030;
+
+const ageRitik = now - 2000;
+const ageDev = now - 2005;
+
+console.log(ageRitik, ageDev, "Hello World");
+console.log(ageDev * 2,  ageDev**2 , ageDev/10, ageDev%10, ageDev+ageRitik);
+
+
+const firstName = "Ritik";
+const birthYear = 2000;
+const nowYear = 2024;
+const job = "Analyst" ;
+
+const details = "I am" + " "+firstName + "a" + (nowYear-birthYear) + "old" + job; 
+console.log(details); // so many difficulties of putting in a string
+
+const detailsNew = `I am ${firstName} a ${nowYear - birthYear} years old ${job}`;
+console.log(detailsNew);
+
+console.log("THIS IS A \n\
+multinline \n\
+string");
+
+console.log(`THIS IS A
+multiline
+string`)
+
