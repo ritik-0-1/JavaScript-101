0c9afc5182243283a5ebdd58d8c845aa99b91c15
ritik-0-1 <ritik.kr775@gmail.com>
2024-05-15 00:24:01 +0530

    1405

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let hasDriverLiscence =  true;
+const passTest = false;
+
+if(passTest) hasDriverLiscence = true;
+if(hasDriverLiscence) console.log("I can drive well :D");
+
+// const interface = 'Audio';
+// console.log(interface);
+
+function logger() {
+    console.log("I am Ritik from inside the logger function");
+}
+// calling, running or envoking the function
+logger();
+// logger();
+// logger();
+
+function fruitProcessor (apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    // console.log(juice) ;l
+    return juice;
+}
+
+console.log(fruitProcessor(5,6)); 
