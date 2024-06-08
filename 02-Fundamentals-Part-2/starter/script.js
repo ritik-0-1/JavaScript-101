074e90469bd920e8b5db13f897502ebf5db7dd89
ritik-0-1 <ritik.kr775@gmail.com>
2024-06-08 21:06:01 +0530

    Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -27,3 +27,28 @@ function fruitProcessor (apples, oranges){
 console.log(fruitProcessor(5,6)); 
 
 console.log(Number('23'));
+
+// Arrow function
+const juiceProcessor= (apples, oranges) =>{
+    return `I want Juice with ${apples} apples and ${oranges} oranges.` ;
+}
+
+console.log(juiceProcessor(3,5));
+
+
+const calcAverage = (score1, score2, score3) => score1+score2+score3/3;
+
+const scoreDolphins = calcAverage(44,23,72); 
+const scoreKoalas =  calcAverage(65,54,49);
+
+function checkWinner ( avgDolphins, avgKoalas){
+    if(avgKoalas >= avgDolphins*2){
+        console.log( `Koalas win (${scoreKoalas} vs. ${scoreDolphins})`);
+    }else if(avgKoalas*2 <= avgDolphins){
+        console.log( `Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
+    }else{
+        console.log(`No team wins...`)
+    }
+}
+
+console.log( checkWinner(scoreDolphins, scoreKoalas));
