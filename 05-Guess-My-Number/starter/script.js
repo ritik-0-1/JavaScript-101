8272feb5a6aa1251b18692f6a9634d491cf98291
ritik-0-1 <ritik.kr775@gmail.com>
2024-06-13 20:43:56 +0530

    startGame

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎆 Start guessing...';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 18;
+// console.log(document.querySelector('.guess').value);
+
+console.log(document.querySelector('.again').textContent);
+
+document.querySelector('.check').addEventListener('click', function () {
+  // document.querySelector('.again').textContent = 'Try Again !!!';
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    // alert('Please enter a valid value');
+    document.querySelector('.message').textContent = 'Please enter a non zero no.';
+  }
+
+});
